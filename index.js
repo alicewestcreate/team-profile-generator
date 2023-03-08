0ced286c586ee4811829255eef0909b4c6a6ec72
Alice <alicewest.create@gmail.com>
2023-03-08 09:32:29 +0000

    refactor

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+<<<<<<< Updated upstream
 const Employee = require("./lib/Employee");
+=======
+>>>>>>> Stashed changes
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+<<<<<<< Updated upstream
+=======
+const path = require("path");
+>>>>>>> Stashed changes
 const fs = require("fs");
 const render = require("./src/page-template.js");
 
@@ -11,6 +18,7 @@ const render = require("./src/page-template.js");
 // const outputPath = path.join(OUTPUT_DIR, "team.html");
 // const { create } = require("domain");
 
+<<<<<<< Updated upstream
 // Ever employee will be asked these three primary questions. 
 const askPrimaryQuest = async function (employeeType) {
   console.log(`Enter ${employeeType}'s Details`);
@@ -127,6 +135,10 @@ const askOptions = async function () {
   ]);
 };
 
+=======
+const render = require("./src/page-template.js");
+const Employee = require("./lib/Employee");
+>>>>>>> Stashed changes
 
 const initiateQuestions = async function () {
 
@@ -178,6 +190,88 @@ const start = async function () {
   });
 };
 
+// const initiateQuestions = function () {
+//   inquirer
+//     .prompt([
+//       {
+//         name: "start_program",
+//         type: "confirm",
+//         message: "Press confirm to start building a team:",
+//       },
+//     ])
+//     .then((answers) => {
+//       console.log(answers);
+//       if (answers.start_program) {
+//         initiateManager();
+//       } else {
+//         console.log("false");
+//       }
+//     });
+// };
 
+<<<<<<< Updated upstream
 start();
+=======
+const askPrimaryQuest = async function (employeeType) {
 
+    console.log(`Enter ${employeeType}'s Details`)
+    return await inquirer.prompt([
+        {
+        name: "name",
+        type: "input",
+        message: "Enter name:",
+        },
+        {
+        name: "id",
+        type: "input",
+        message: "Enter ID:",
+        },
+        {
+        name: "email",
+        type: "input",
+        message: "Enter email:",
+        },
+    ]);
+};
+>>>>>>> Stashed changes
+
+
+const managerInfo = async function () {
+    const manager = "manager"
+    const primaryAnswer = await askPrimaryQuest(manager);
+    var answers = await inquirer.prompt([
+        {
+        name: "officeNum",
+        type: "input",
+        message: "Enter office number:",
+        },
+    ]);
+
+    const managerObj = new Manager(
+        primaryAnswer.name,
+        primaryAnswer.id,
+        primaryAnswer.email,
+        answers.officeNum
+    );
+    console.log(managerObj);
+  nextAction();
+};
+
+const nextAction = function () {};
+managerInfo()
+// initiateQuestions();
+
+
+// Prompt:
+// Create Team Profile
+// Confirm Y/N
+// Enter Managers Details
+// Name, ID, Email & Office Number
+
+// Would you like to build a team:
+// Yes or No
+
+// Select one of the follow:
+// Add an Engineer
+// Add an Intern
+// Finish building the team
